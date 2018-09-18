c9b10b9091a89ca9504607641e786d3d70047c6e
KhanhDat <keepyoursmile.512@gmail.com>
2018-09-18 16:31:32 +0700

    them linh tinh

--- Assets/_Scripts/JsonController/GameConfig.cs
+++ Assets/_Scripts/JsonController/GameConfig.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.GameCenter;
+using com.shephertz.app42.paas.sdk.csharp;
+using com.shephertz.app42.paas.sdk.csharp.storage;
 
 public class GameConfig : MonoBehaviour
 {
     public static GameConfig Instance;
+    public static string id = "";
     void Awake()
     {
         if (Instance != null)
@@ -12,6 +16,26 @@ public class GameConfig : MonoBehaviour
             return;
         }
         Instance = this;
+    }
+
+    void Start()
+    {
+        if (id == "")
+        {
+            App42API.Initialize("70d071010820cea793a6fecd4a573a7551c630da18878944b24d09b4a50c1212", "48faebb001dc22c3d0b453bcc351fe45120e3f9660ef458f939c9f2055ee352e");
+            GameCenterPlatform.ShowDefaultAchievementCompletionBanner(true);
+            Social.localUser.Authenticate(success =>
+            {
+                if (success)
+                {
+                    id = Social.localUser.id;
+                    StorageService storageService = App42API.BuildStorageService();
+                    storageService.FindDocumentByKeyValue("Db", "Data", "id", id, new UnityCallBack1());
+                }
+                else
+                    Debug.Log("Failed to authenticate");
+            });
+        }
     }
 
     public int goldToDollar;
@@ -35,4 +59,18 @@ public class GameConfig : MonoBehaviour
     public float captruckIndex;
     public string idInter_android;
     public string idInter_ios;
+    public string kProductID50 = "consumable";
+    public string kProductID300 = "consumable";
+    public string kProductID5000 = "consumable";
+}
+
+public class SaveGold
+{
+    public string id;
+    public int gold;
+    public SaveGold(string id, int gold)
+    {
+        this.id = id;
+        this.gold = gold;
+    }
 }
