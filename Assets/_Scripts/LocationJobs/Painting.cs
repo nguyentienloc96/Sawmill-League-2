b326905b3c080dfbfc7d93f22083a1ba0cc73918
DESKTOP-8TJOP7T\nguye <nguyenloc271096@gmail.com>
2018-10-01 11:46:19 +0700

    push

--- Assets/_Scripts/LocationJobs/Painting.cs
+++ Assets/_Scripts/LocationJobs/Painting.cs
@@ -1,105 +1,77 @@
 ﻿using UnityEngine;
 using DG.Tweening;
+using System.Collections.Generic;
+
+[System.Serializable]
+public struct Way
+{
+    public Transform[] arrTf;
+    //[HideInInspector]
+    public Vector3[] arrDis;
+}
 
 public class Painting : MonoBehaviour
 {
-    public bool isInput;
+
     public Transform cart;
-    public Transform tree;
-    public GameObject notification;
-    public Animator anim;
+    public Transform pen;
 
-    public SpriteRenderer imgHand;
+    public List<Way> way;
 
-    private bool isRun;
-    private Vector3 posDown;
-    private Vector3 posCheck;
+    public int index;
 
     public void Start()
     {
-        posCheck = transform.GetChild(0).position;
-    }
-
-    private void OnEnable()
-    {
-        int ID = GameManager.Instance.IDLocation;
-        int IndexType = GameManager.Instance.lsLocation[ID].indexType;
-        if (GameManager.Instance.lsLocation[ID].lsWorking[IndexType].input > 0)
+        index = 0;
+        foreach(Way w in way)
         {
-            notification.SetActive(false);
-            tree.gameObject.SetActive(true);
-            LoadInput();
-        }
-        else
-        {
-            tree.gameObject.SetActive(false);
-            notification.SetActive(true);
-        }
-    }
-
-    public void Update()
-    {
-        if (isRun)
-        {
-            if (Input.mousePosition.y > posDown.y)
+            for(int i = 0;i< w.arrTf.Length; i++)
             {
-                float dis = Input.mousePosition.y - posDown.y;
-                cart.position += new Vector3(0f, dis * 0.01f * Time.deltaTime, 0f);
-            }
-            if (cart.position.y > posCheck.y)
-            {
-                CompleteJob();
+                w.arrDis[i] = w.arrTf[i].localPosition;
             }
         }
+        pen.localPosition = way[0].arrDis[0];
+        cart.DOLocalMove(Vector3.zero, 1f);
+        pen.DOLocalPath(way[0].arrDis,2f);
     }
 
-    public void TapDown()
+    public void TapDownPen()
     {
-        if (isInput)
-        {
-            anim.enabled = true;
-            imgHand.sprite = UIManager.Instance.spHand[0];
-            AudioManager.Instance.Play("Debarking");
-            posDown = Input.mousePosition;
-            isRun = true;
-        }
-    }
+        index++;
+        //pen.DOLocalMove(arrPos[index], 1f).OnComplete(() =>
+        //{
+        //    if ((index + 1) < arrPos.Length)
+        //    {
+        //        if (pen.localPosition.y == arrPos[index + 1].y)
+        //        {
+        //            pen.localEulerAngles = new Vector3(0f, 0f, -90f);
+        //        }
+        //        else
+        //        {
+        //            if (pen.localPosition.y < arrPos[index + 1].y)
+        //            {
+        //                pen.localEulerAngles = new Vector3(0f, 0f, 0f);
+        //            }
+        //            else
+        //            {
+        //                pen.localEulerAngles = new Vector3(0f, 0f, -180f);
+        //            }
+        //        }
+        //    }
+        //    else
+        //    {
+        //        cart.localPosition = new Vector3(-10f, 0f, 0f);
+        //        LoadInput();
+        //    }
+        //});
 
-    public void TapUp()
-    {
-        anim.enabled = false;
-        imgHand.sprite = UIManager.Instance.spHand[1];
-        AudioManager.Instance.Stop("Debarking");
-        isRun = false;
     }
 
     public void LoadInput()
     {
-        cart.localPosition = new Vector3(-4f, 0f, 0f);
-        cart.DOLocalMove(Vector3.zero, 1f).OnComplete(() =>
-        {
-            isInput = true;
-            imgHand.enabled = true;
-        });
-    }
-
-    public void CompleteJob()
-    {
-        anim.enabled = false;
-        isRun = false;
-        int ID = GameManager.Instance.IDLocation;
-        int IndexType = GameManager.Instance.lsLocation[ID].indexType;
-        GameManager.Instance.lsLocation[ID].JobComplete(IndexType);
-        if (GameManager.Instance.lsLocation[ID].lsWorking[IndexType].input > 0)
-        {
-            imgHand.enabled = false;
-            isInput = false;
-            LoadInput();
-        }
-        else
-        {
-            tree.gameObject.SetActive(false);
-            notification.SetActive(true);
-        }
+        index = 0;
+        //pen.localPosition = arrPos[index];
+        pen.localEulerAngles = new Vector3(0f, 0f, -90f);
+        cart.DOLocalMove(Vector3.zero, 1f);
     }
 }
---
+++ Assets/_Scripts/LocationJobs/TapDown.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TapDown : MonoBehaviour {
+
+    public UnityEvent onMouseDown;
+
+    public void OnMouseDown()
+    {
+        onMouseDown.Invoke();
+    }
+
+}
