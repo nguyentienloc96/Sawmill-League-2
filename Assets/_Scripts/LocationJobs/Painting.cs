a692320f824205c0ff0cf55261672507b62e5860
DESKTOP-8TJOP7T\nguye <nguyenloc271096@gmail.com>
2018-10-02 07:29:43 +0700

    ok Painting

--- Assets/_Scripts/LocationJobs/Painting.cs
+++ Assets/_Scripts/LocationJobs/Painting.cs
@@ -3,75 +3,176 @@ using DG.Tweening;
 using System.Collections.Generic;
 
 [System.Serializable]
-public struct Way
+public struct Cart
 {
-    public Transform[] arrTf;
-    //[HideInInspector]
-    public Vector3[] arrDis;
+    public GameObject[] gTree;
+    public SpriteRenderer[] spTree;
 }
 
 public class Painting : MonoBehaviour
 {
 
+    public bool isInput;
     public Transform cart;
     public Transform pen;
+    public Animator anim;
+    public GameObject notification;
 
-    public List<Way> way;
 
-    public int index;
+    public List<Transform> way;
+    public List<Cart> lsCart;
 
-    public void Start()
+    private bool isRun;
+    private Vector3 posDown;
+    private int random;
+    private int indexPos;
+    private int indexTree;
+
+
+    private void OnEnable()
     {
-        index = 0;
-        foreach(Way w in way)
+        int ID = GameManager.Instance.IDLocation;
+        int IndexType = GameManager.Instance.lsLocation[ID].indexType;
+        if (GameManager.Instance.lsLocation[ID].lsWorking[IndexType].input > 0)
         {
-            for(int i = 0;i< w.arrTf.Length; i++)
-            {
-                w.arrDis[i] = w.arrTf[i].localPosition;
-            }
+            notification.SetActive(false);
+            LoadInput();
         }
-        pen.localPosition = way[0].arrDis[0];
-        cart.DOLocalMove(Vector3.zero, 1f);
-        pen.DOLocalPath(way[0].arrDis,2f);
+        else
+        {
+            notification.SetActive(true);
+        }
     }
 
-    public void TapDownPen()
+    public void Update()
     {
-        index++;
-        //pen.DOLocalMove(arrPos[index], 1f).OnComplete(() =>
-        //{
-        //    if ((index + 1) < arrPos.Length)
-        //    {
-        //        if (pen.localPosition.y == arrPos[index + 1].y)
-        //        {
-        //            pen.localEulerAngles = new Vector3(0f, 0f, -90f);
-        //        }
-        //        else
-        //        {
-        //            if (pen.localPosition.y < arrPos[index + 1].y)
-        //            {
-        //                pen.localEulerAngles = new Vector3(0f, 0f, 0f);
-        //            }
-        //            else
-        //            {
-        //                pen.localEulerAngles = new Vector3(0f, 0f, -180f);
-        //            }
-        //        }
-        //    }
-        //    else
-        //    {
-        //        cart.localPosition = new Vector3(-10f, 0f, 0f);
-        //        LoadInput();
-        //    }
-        //});
+        if (isRun)
+        {
+            if ((indexPos + 1) < way.Count)
+            {
+                if (way[indexPos].localPosition.y == way[indexPos + 1].localPosition.y)
+                {
+                    if (Input.mousePosition.x > posDown.x)
+                    {
+                        float dis = Input.mousePosition.x - posDown.x;
+                        pen.localPosition += new Vector3(dis * 0.01f * Time.deltaTime, 0f, 0f);
+                        if (pen.localPosition.x >= way[indexPos + 1].localPosition.x)
+                        {
+                            pen.localPosition = way[indexPos + 1].localPosition;
+                            if (indexPos == 1 || indexPos == 2 || indexPos == 5 || indexPos == 6 || indexPos == 9 || indexPos == 10)
+                            {
+                                lsCart[indexTree].spTree[random].color = new Color32(255, 255, 255, 255);
+                                indexTree++;
+                            }
+                            indexPos++;
+                        }
+                    }
+                    pen.localEulerAngles = new Vector3(0f, 0f, -90f);
+                }
+                else
+                {
+                    if (way[indexPos].localPosition.y > way[indexPos + 1].localPosition.y)
+                    {
+                        if (Input.mousePosition.y < posDown.y)
+                        {
+                            float dis = Input.mousePosition.y - posDown.y;
+                            pen.localPosition += new Vector3(0f, dis * 0.01f * Time.deltaTime, 0f);
+                            if (pen.localPosition.y <= way[indexPos + 1].localPosition.y)
+                            {
+                                pen.localPosition = way[indexPos + 1].localPosition;
+                                if (indexPos == 1 || indexPos == 2 || indexPos == 5 || indexPos == 6 || indexPos == 9 || indexPos == 10)
+                                {
+                                    lsCart[indexTree].spTree[random].color = new Color32(255, 255, 255, 255);
+                                    indexTree++;
+                                }
+                                indexPos++;
+                            }
+                        }
+                        pen.localEulerAngles = new Vector3(0f, 0f, -180f);
+                    }
+                    else if (way[indexPos].localPosition.y < way[indexPos + 1].localPosition.y)
+                    {
+                        if (Input.mousePosition.y > posDown.y)
+                        {
+                            float dis = Input.mousePosition.y - posDown.y;
+                            pen.localPosition += new Vector3(0f, dis * 0.01f * Time.deltaTime, 0f);
+                            if (pen.localPosition.y >= way[indexPos + 1].localPosition.y)
+                            {
+                                pen.localPosition = way[indexPos + 1].localPosition;
+                                if (indexPos == 1 || indexPos == 2 || indexPos == 5 || indexPos == 6 || indexPos == 9 || indexPos == 10)
+                                {
+                                    lsCart[indexTree].spTree[random].color = new Color32(255, 255, 255, 255);
+                                    indexTree++;
+                                }
+                                indexPos++;
+                            }
+                        }
+                        pen.localEulerAngles = new Vector3(0f, 0f, 0f);
+                    }
+                }
+            }
+            else
+            {
+                CompleteJob();
+            }
+        }
+    }
 
+    public void TapDown()
+    {
+        if (isInput)
+        {
+            anim.enabled = true;
+            AudioManager.Instance.Play("Debarking");
+            posDown = Input.mousePosition;
+            isRun = true;
+        }
+    }
+
+    public void TapUp()
+    {
+        anim.enabled = false;
+        AudioManager.Instance.Stop("Debarking");
+        isRun = false;
     }
 
     public void LoadInput()
     {
-        index = 0;
-        //pen.localPosition = arrPos[index];
-        pen.localEulerAngles = new Vector3(0f, 0f, -90f);
-        cart.DOLocalMove(Vector3.zero, 1f);
+        random = Random.Range(0, 4);
+        for(int i = 0; i < lsCart.Count; i++)
+        {
+            lsCart[i].gTree[random].SetActive(true);
+        }
+        indexTree = 0;
+        indexPos = 0;
+        cart.localPosition = new Vector3(-10f, 0f, 0f);
+        pen.localPosition = way[indexPos].localPosition;
+        cart.DOLocalMove(Vector3.zero, 0.5f).OnComplete(() =>
+        {
+            isInput = true;
+        });
+    }
+
+    public void CompleteJob()
+    {
+        for (int i = 0; i < lsCart.Count; i++)
+        {
+            lsCart[i].gTree[random].SetActive(false);
+            lsCart[i].spTree[random].color = new Color32(255, 255, 255, 0);
+        }
+        anim.enabled = false;
+        isRun = false;
+        int ID = GameManager.Instance.IDLocation;
+        int IndexType = GameManager.Instance.lsLocation[ID].indexType;
+        GameManager.Instance.lsLocation[ID].JobComplete(IndexType);
+        if (GameManager.Instance.lsLocation[ID].lsWorking[IndexType].input > 0)
+        {
+            isInput = false;
+            LoadInput();
+        }
+        else
+        {
+            notification.SetActive(true);
+        }
     }
 }
